ada622d8ff8bfa763c605af5f05a056e1d7cb84d
Venkatesh4697 <40483195+Venkatesh4697@users.noreply.github.com>
2023-01-11 17:27:44 +0530

    Auth Changes
    Added Authentication changes for the APIs

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import binascii
+import datetime
+import json
 import logging
+import os
 
 import flask
 from flask import Flask
@@ -11,7 +15,9 @@ from bson.objectid import ObjectId
 
 from flask import jsonify, request
 
-from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+
+from dotenv import load_dotenv
 
 from flask_cors import CORS
 
@@ -19,9 +25,9 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.secret_key = "secretkey"
+load_dotenv('/.env')
 
-client = MongoClient("mongodb+srv://venky:venky97@cloudmongo101.0zqn1.mongodb.net/?retryWrites=true")
+client = MongoClient(os.environ.get("DB_CON_STRING"))
 
 db = client.get_database("film_db")
 
@@ -37,54 +43,119 @@ def add_film():
     replacement_cost = _json['replacement_cost']
     rating = _json['rating']
     list_actors = _json['list_actors']
+    if 'Authorization' in request.headers:
+        try:
+            token = request.headers['Authorization'].split(" ")
+            auth = apiAuth(token[1])
+            # print(auth)
+            if 'token' in auth.keys():
+                if title and desc and rental_duration and rental_rate and length and replacement_cost and rating and list_actors and request.method == 'PATCH':
+                    film = db.film.insert_one({'title': title,
+                                               'description': desc,
+                                               'rental_duration': rental_duration,
+                                               'rental_rate': rental_rate,
+                                               'length': length,
+                                               'replacement_cost': replacement_cost,
+                                               'rating': rating,
+                                               'list_actors': list_actors})
+                    resp = jsonify("Film Inserted successfully.")
+                    resp.status_code = 200
+                    if film is None:
+                        return not_found()
+                    else:
+                        return resp
+            else:
+                return invalid_token()
+        except jwt.exceptions.InvalidSignatureError:
+            return invalid_token()
+        except jwt.exceptions.ExpiredSignatureError:
+            return token_expired()
+        except binascii.Error:
+            return invalid_token()
 
-    if title and desc and rental_duration and rental_rate and length and replacement_cost and rating and list_actors and request.method == 'POST':
-        id = db.film.insert_one({'title': title,
-                                 'description': desc,
-                                 'rental_duration': rental_duration,
-                                 'rental_rate': rental_rate,
-                                 'length': length,
-                                 'replacement_cost': replacement_cost,
-                                 'rating': rating,
-                                 'list_actors': list_actors})
-
-        resp = jsonify("Film inserted successfully.")
-
-        resp.status_code = 200
-        return resp
     else:
-        return not_found()
+        return unauthorised_error()
 
 
 @app.route("/api", methods=['GET'])
 def get_films():
-    films = db.film.find()
-    resp = dumps(films)
-    if films is None:
-        return not_found()
+    if 'Authorization' in request.headers:
+        try:
+            token = request.headers['Authorization'].split(" ")
+            auth = apiAuth(token[1])
+            # print(auth)
+            if 'token' in auth.keys():
+                films = db.film.find()
+                resp = dumps(films)
+                if films is None:
+                    return not_found()
+                else:
+                    return resp
+            else:
+                return invalid_token()
+        except jwt.exceptions.InvalidSignatureError:
+            return invalid_token()
+        except jwt.exceptions.ExpiredSignatureError:
+            return token_expired()
+        except jwt.exceptions.DecodeError:
+            return invalid_token()
+
     else:
-        return resp
+        return unauthorised_error()
 
 
 @app.route("/api/<string:fname>", methods=['GET'])
 def get_film(fname):
-    film = db.film.find_one({'title': fname})
-    resp = dumps(film)
-    if film is None:
-        return not_found()
+    if 'Authorization' in request.headers:
+        try:
+            token = request.headers['Authorization'].split(" ")
+            auth = apiAuth(token[1])
+            # print(auth)
+            if 'token' in auth.keys():
+                film = db.film.find_one({'title': fname})
+                resp = dumps(film)
+                if film is None:
+                    return not_found()
+                else:
+                    return resp
+            else:
+                return invalid_token()
+        except jwt.exceptions.InvalidSignatureError:
+            return invalid_token()
+        except jwt.exceptions.ExpiredSignatureError:
+            return token_expired()
+        except jwt.exceptions.DecodeError:
+            return invalid_token()
+
     else:
-        return resp
+        return unauthorised_error()
 
 
 @app.route("/api/<string:fname>", methods=['DELETE'])
 def delete_film(fname):
-    film = db.film.delete_one({'title': fname})
+    if 'Authorization' in request.headers:
+        try:
+            token = request.headers['Authorization'].split(" ")
+            auth = apiAuth(token[1])
+            # print(auth)
+            if 'token' in auth.keys():
+                film = db.film.delete_one({'title': fname})
+                resp = jsonify("Film " + fname + " Deleted successfully.")
+                if film is None:
+                    return not_found()
+                else:
+                    return resp
+            else:
+                return invalid_token()
+        except jwt.exceptions.InvalidSignatureError:
+            return invalid_token()
+        except jwt.exceptions.ExpiredSignatureError:
+            return token_expired()
+        except jwt.exceptions.DecodeError:
+            return invalid_token()
 
-    resp = jsonify("Film " + fname + " Deleted successfully.")
-    if film is None:
-        return not_found()
     else:
-        return resp
+        return unauthorised_error()
 
 
 @app.route("/api/<string:fname>", methods=['PATCH'])
@@ -99,22 +170,39 @@ def update_film(fname):
     rating = _json['rating']
     list_actors = _json['list_actors']
 
-    if title and desc and rental_duration and rental_rate and length and replacement_cost and rating and list_actors and request.method == 'PATCH':
-        db.film.update_one({'title': fname},
-                           {'$set': {'description': desc,
-                                     'rental_duration': rental_duration,
-                                     'rental_rate': rental_rate,
-                                     'length': length,
-                                     'replacement_cost': replacement_cost,
-                                     'rating': rating,
-                                     'list_actors': list_actors}
-                            })
-        resp = jsonify("Film Updated successfully.")
+    if 'Authorization' in request.headers:
+        try:
+            token = request.headers['Authorization'].split(" ")
+            auth = apiAuth(token[1])
+            # (auth)
+            if 'token' in auth.keys():
+                if title and desc and rental_duration and rental_rate and length and replacement_cost and rating and list_actors and request.method == 'PATCH':
+                    film = db.film.update_one({'title': fname},
+                                              {'$set': {'description': desc,
+                                                        'rental_duration': rental_duration,
+                                                        'rental_rate': rental_rate,
+                                                        'length': length,
+                                                        'replacement_cost': replacement_cost,
+                                                        'rating': rating,
+                                                        'list_actors': list_actors}
+                                               })
+                    resp = jsonify("Film Updated successfully.")
+                    resp.status_code = 200
+                    if film is None:
+                        return not_found()
+                    else:
+                        return resp
+            else:
+                return invalid_token()
+        except jwt.exceptions.InvalidSignatureError:
+            return invalid_token()
+        except jwt.exceptions.ExpiredSignatureError:
+            return token_expired()
+        except jwt.exceptions.DecodeError:
+            return invalid_token()
 
-        resp.status_code = 200
-        return resp
     else:
-        return not_found()
+        return unauthorised_error()
 
 
 @app.errorhandler(404)
@@ -126,6 +214,80 @@ def not_found(error=None):
     resp = jsonify(message)
     resp.status_code = 404
     return resp
+
+
+@app.errorhandler(401)
+def unauthorised_error(error=None):
+    message = {
+        'status': 401,
+        'message': 'HTTP 401 - Unauthorised'
+    }
+    resp = jsonify(message)
+    resp.status_code = 401
+    return resp
+
+
+def invalid_token(error=None):
+    message = {
+        'status': 401,
+        'message': 'HTTP 401 - Invalid Auth Token.'
+    }
+    resp = jsonify(message)
+    resp.status_code = 401
+    return resp
+
+
+def invalid_key(error=None):
+    message = {
+        'status': 401,
+        'message': 'HTTP 401 - Invalid API Key. Please contact the administrator.'
+    }
+    resp = jsonify(message)
+    resp.status_code = 401
+    return resp
+
+
+def token_expired(error=None):
+    message = {
+        'status': 401,
+        'message': 'HTTP 401 - Authorization Token Expired.'
+    }
+    resp = jsonify(message)
+    resp.status_code = 401
+    return resp
+
+
+def apiAuth(token):
+    api_secret = os.environ.get('API_SECRET')
+    # print(token)
+    # print(api_secret)
+    json = jwt.decode(token, api_secret, algorithms=["HS256"])
+    return json
+
+
+@app.route("/auth/v1/tokens", methods=['GET'])
+def generate_token():
+    if 'Authorization' in request.headers:
+        bearer = request.headers['Authorization'].split(" ")[1]
+        tok_request = jsonify({'token': bearer})
+        api_secret = os.environ.get('API_SECRET')
+        db_tok = db.tokens.find_one({'token': str(bearer)})
+        # print(db_tok)
+        if db_tok is not None:
+            token = jwt.encode({"token": str(bearer),
+                                "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=2)},
+                               api_secret, algorithm="HS256")
+            message = {
+                'status': 200,
+                'token': token
+            }
+            resp = jsonify(message)
+            resp.status_code = 200
+            return resp
+        else:
+            return invalid_key()
+    else:
+        return unauthorised_error()
 
 
 if __name__ == '__main__':
